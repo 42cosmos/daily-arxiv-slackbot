5b5fd791088e5dacead7bbbc0e82ee1a095382f5
Eunbin Park <parkeb417@gmail.com>
2023-04-11 18:30:14 +0900

    APIError logging msg 추가

--- openai_chatgpt.py
+++ openai_chatgpt.py
@@ -30,6 +30,7 @@ class OpenAIGpt:
 
         except openai.error.APIError as e:
             logger.exception(f"API Error: {e}")
+            logger.exception(f"Status Code: {e.status_code}, Response: {e.response.json()}")
             return False
 
         except Exception as e:
